250e37dd61bb86416beb48881e001ec1d4a50eb3
Arkadi Kagan <arkadi_kagan@hotmail.com>
2025-12-29 00:21:27 +0200

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.channelplayer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
